7573a203f9d0c62df696e5484fc58da82701b16e
hongran997 <144437360+hongran997@users.noreply.github.com>
2024-08-23 13:46:31 +0000

    Deploying to gh-pages from @ hongran997/react-hooks@95abc6c380fc7a630d0d8c2c7d8a923d6cdba1a0 🚀

--- RADEME.md
+++
@@ -1,6 +0,0 @@
-因为 webpack 的缘故， 最高支持 node16
-
-npm run init
-
-npm run test
-
--- packages/hooks/src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import * as hooks from '..';
-
-describe('encode-hooks', () => {
-  test('exports modules should be defined', () => {
-    Object.keys(hooks).forEach((module) => {
-      expect(hooks[module]).toBeDefined();
-    });
-  });
-});

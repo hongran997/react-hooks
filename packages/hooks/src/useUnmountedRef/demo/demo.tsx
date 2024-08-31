bb3dceafab6e5671a19e1d73fa1a8de00821a8b2
hongran997 <zhaohongran1997@163.com>
2024-08-31 14:36:45 +0800

    feat: add useUnmountedRef desc

--- packages/hooks/src/useUnmountedRef/demo/demo.tsx
+++ packages/hooks/src/useUnmountedRef/demo/demo.tsx
@@ -6,6 +6,11 @@ import { message } from 'antd';
 function MyComponent() {
   const unmountedRef = useUnmountedRef();
 
+  /**
+   * 父组件的渲染函数和 useEffect 先于子组件执行。
+   * 同级组件之间，按照在代码中的顺序依次执行。
+   * 同一组件内的多个 useEffect 按照在代码中的顺序依次执行。
+   */
   useEffect(() => {
     setTimeout(() => {
       if (!unmountedRef.currect) {
